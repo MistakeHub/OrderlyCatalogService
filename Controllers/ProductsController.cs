4038b7128f31ddf2f654fd6aecf4f8fa6185184c
MistakeHub <kirill.cherkez@mail.ru>
2025-10-09 10:44:55 +0300

    Add postgresSQL support through docker

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Orderly.Catalog.Database;
+
+namespace Orderly.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        CatalogDbContext _db;
+        public ProductsController(CatalogDbContext db) { _db = db; }
+
+        [HttpGet("test-db")]
+        public async Task<IActionResult> TestDb()
+        {
+            var count = await _db.Products.CountAsync();
+            return Ok($"Products count: {count}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using Orderly.Catalog.Database;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-
+builder.Services.AddDbContext<CatalogDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("CatalogDb")));
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
@@ -18,6 +21,23 @@ builder.Services.AddCors(options =>
     });
 });
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<CatalogDbContext>();
+
+    try
+    {
+        Console.WriteLine("Applying database migrations...");
+        dbContext.Database.Migrate();
+        Console.WriteLine("Database migration complete!");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Database migration failed: " + ex.Message);
+        throw; 
+    }
+}
 
 // Configure the HTTP request pipeline.
 
